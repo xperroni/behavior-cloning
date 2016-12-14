e37dab0ac0641884790a72a11f6fc0628f322300
Helio Perroni Filho <mail@xperroni.me>
2016-12-14 19:49:45 -0200

    Regresion model tests
    Signed-off-by: Helio Perroni Filho <mail@xperroni.me>

--- train_regression.sh
+++ train_regression.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-python model.py datasets/track1/middle06/driving_log.csv datasets/track1/right01/recovery.csv datasets/track1/left01/recovery.csv --architecture regression
+python model.py \
+    datasets/track1/middle06/driving_log.csv \
+    datasets/track1/right01/recovery.csv \
+    datasets/track1/left01/recovery.csv \
+    --architecture regression
